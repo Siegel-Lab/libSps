c2fcbd0ca15e6aef859baa2b1458630d98d73642
Markus Schmidt <markus.rainer.schmidt@gmail.com>
2022-02-25 10:45:19 +0100

    tryinig other split approach

--- count_size.py
+++ count_size.py
@@ -19,7 +19,7 @@ def parse_heatmap(in_filename, compression):
             yield chr_1, pos_1//compression, chr_2, pos_2//compression, max(mapq_1, mapq_2)
 
 
-def input_to_points(len_file_name, heatmap_filenames, limit=float('inf'), compression=100):
+def input_to_points(len_file_name, heatmap_filenames, limit=float('inf'), compression=1000):
     curr_start = 0
     chr_start_pos = {}
     with open(len_file_name, "r") as len_file:
@@ -27,23 +27,24 @@ def input_to_points(len_file_name, heatmap_filenames, limit=float('inf'), compre
             chr_name, chr_len = line.split("\t")
             chr_start_pos[chr_name] = curr_start
             curr_start += int(chr_len)//compression
-    points_up = []
-    points_right = []
+    points = []
     cnt = 0
-    for file_name in heatmap_filenames:
+    for idx, file_name in enumerate(heatmap_filenames):
+        x = []
+        for _ in range(11):
+            x.append([])
+        points.append(x)
         for chr_1, pos_1, chr_2, pos_2, map_q in parse_heatmap(file_name, compression):
             x = chr_start_pos[chr_1] + int(pos_1)
             y = chr_start_pos[chr_2] + int(pos_2)
             max_dist_betw_alignments = random.choice(list(range(10)))
-            annotations = [True] + \
-                [random.choice([True, False]) for _ in range(10)]
-            p = points_up if y > x else points_right
-            for a, _ in enumerate(annotations):
-                p.append((map_q, max_dist_betw_alignments, a, x, y))
+            a = random.choice(range(10))
+            points[-1][a+1].append((map_q, max_dist_betw_alignments, x, y))
+            points[-1][0].append((map_q, max_dist_betw_alignments, x, y))
             cnt += 1
             if cnt > limit:
-                return points_up, points_right, False
-    return points_up, points_right, True
+                return points, False
+    return points, True
 
 
 def product(l):
@@ -109,15 +110,18 @@ def mem(points, axes):
     for p in points:
         for i, x in enumerate(p):
             s[i].add(x)
+    #print(points, axes)
+    #assert (len(points) > 0) == (len(axes) > 0)
     # the position in the dimension and the continuous sum
     # also the bottom left position of the bin
-    #overlay_size = sum(len(x) for x in axes) * 2 + len(axes)
-    overlay_size = sum(max(x)-min(x) for x in axes) + len(axes) * 2
-    overlay_fill = sum(len(x) for x in axes) / sum(max(x)-min(x) for x in axes)
+    overlay_size = sum(len(x)-1 for x in axes) * 2 + len(axes) + 2 + 1
+    overlay_size_2 = sum(max(x)-min(x) if len(x) > 0 else 0 for x in axes) + len(axes) * 2 + 1
+    overlay_fill = sum(len(x) for x in axes) / sum(max(x)-min(x) if len(x) > 0 else 0 for x in axes)
     # the position = d-long vec, the cont sum value and the pointer to the description
-    intersections = product(len(x) for x in s)//2 + 1
-    cont_sum_size = (intersections + len(set(points))) * (len(axes) + 2)
-    return overlay_size, cont_sum_size, intersections, overlay_fill
+    intersections = product(len(x) for x in s)//2 + 1 + len(set(points))
+    intersections_size = intersections * (len(axes) + 1)
+    points_size = len(points) * 2
+    return min(overlay_size, overlay_size_2), points_size, intersections_size, intersections, overlay_fill
 
 
 def compute_mem(points, axes=None):
@@ -127,52 +131,63 @@ def compute_mem(points, axes=None):
             axes.append(list(set(p[d] for p in points)))
     if len(points) == 0:
         return 0, 0, 0
-    overlay_size, cont_sum_size, intersections, overlay_fill = mem(
+    overlay_size, points_size, intersections_size, intersections, overlay_fill = mem(
         points, axes)
     split = split_points(points, axes)
     split_size = 0
     for points_split, split_axis in split:
-        split_overlay_size, split_cont_sum_size, _, _ = mem(
+        split_overlay_size, split_points_size, split_intersections_size, _, _ = mem(
             points_split, split_axis)
-        split_size += split_overlay_size + split_cont_sum_size
-    if overlay_size + cont_sum_size <= split_size:
+        split_size += split_overlay_size + split_points_size + split_intersections_size
+    if overlay_size + points_size + intersections_size <= split_size:
         #start = [min(p[d] for p in points) for d in range(len(points[0]))]
         #end = [max(p[d] for p in points) for d in range(len(points[0]))]
         #print("overlay with", len(points), "points")
-        return overlay_size, cont_sum_size, 1, len(points), intersections, overlay_fill
+        return overlay_size, points_size, intersections_size, 1, len(points), intersections, overlay_fill
     else:
         del points
         del axes
         split_overlay_size = 0
-        split_cont_sum_size = 0
+        split_points_size = 0
         split_points_sum = 0
+        split_intersections_sum = 0
         num_overlays = 0
         split_intersections = 0
         split_overlay_fill = 0
         for points_split, split_axis in split:
-            a, b, c, d, e, f = compute_mem(points_split, split_axis)
+            a, b, g, c, d, e, f = compute_mem(points_split, split_axis)
             split_overlay_size += a
-            split_cont_sum_size += b
+            split_points_size += b
+            split_intersections_sum += g
             split_points_sum += d
             num_overlays += c
             split_intersections += e
             split_overlay_fill += f
-        return split_overlay_size, split_cont_sum_size, num_overlays, split_points_sum/len(split), \
-            split_intersections/len(split), split_overlay_fill/len(split)
+        return split_overlay_size, split_points_size, split_intersections_sum, num_overlays, \
+               split_points_sum/len(split), split_intersections/len(split), split_overlay_fill/len(split)
 
+def sum_mem(points_l):
+    l = []
+    for ps in points_l:
+        for p in ps:
+            l.append(compute_mem(p))
+    r = [sum(p[x] for p in l) for x in range(len(l[0]))]
+    for x in range(4,7):
+        r[x] = r[x] / len(l)
+    return r
 
-EXMAPLE = [
+EXMAPLE = [[[
     (1, 1),
     (2, 2),
     (3, 3),
-]
-EXMAPLE_2 = [
+]]]
+EXMAPLE_2 = [[[
     (1, 1),
     (3, 1),
     (2, 3),
     (3, 4),
     (4, 3),
-]
+]]]
 
 BED_FOLDER = "/work/project/ladsie_012/ABS.2.2/2021-10-26_NS502-NS521_ABS_CR_RADICL_inputMicroC/bed_files"
 BED_SUFFIX = "RNA.sorted.bed_K1K2.bed_K4.bed_R_D.bed_R_D_K1K2.bed_R_D_PRE1.bed"
@@ -184,18 +199,23 @@ FILES = [
 ]
 
 if __name__ == "__main__":
+    #sum_mem(EXMAPLE_2)
+    #exit()
     for x in range(3, 100):
         print(10**x, "points:")
-        points_up, points_right, have_all = input_to_points(
+        points, have_all = input_to_points(
             "../out/Lister427.sizes", FILES, limit=10**x)
-        a, b, c, d, e, f = compute_mem(points_up + points_right)
-        print("\ttotal of", a, "overlay integers = ", 100*a/(a+b), "%")
-        print("\ttotal of", b, "cont sum integers = ", 100*b/(a+b), "%")
+        a, b, g, c, d, e, f = sum_mem(points)
+        print("\ttotal of", a, "overlay integers = ", 100*a/(a+b+g), "%")
+        print("\ttotal of", b, "points integers = ", 100*b/(a+b+g), "%")
+        print("\ttotal of", g, "intersection integers = ", 100*g/(a+b+g), "%")
         print("\ttotal of", c, "overlays")
         print("\taverage of", d, "points per overlay")
         print("\taverage of", e, "intersections per overlay")
         print("\taverage overlay fill: ", 100*f, "%")
-        print("\ttotal of", 8 * (a+b) / 10**9, "gb")
+        print("\ttotal of", 8 * (a+b+g) / 10**9, "gb")
+        print("\tOR total of", 8 * (a+b) / 10**9, "gb with runtime penalty of factor", 1+d/math.log2(d+e),
+                "memory reduced to:", 100*((a+b)/(a+b+g)), "%")
         if have_all:
             break
 
